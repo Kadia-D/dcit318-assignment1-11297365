327a9a4d109973ae39ae8f781f0166556c14fc61
Kadia-D <kddellor@gmail.com>
2025-07-31 20:52:36 +0000

    ticket price

---
+++ TicketPriceCalculatorApp/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Grade
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        if (int.TryParse(Console.ReadLine(), out int age))
+        {
+            if (age <= 12 || age >= 65)
+                Console.WriteLine("Ticket Price: GHC7 (Discounted)");
+            else
+                Console.WriteLine("Ticket Price: GHC10");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+        }
+    }
+}
---
+++ TriangleID/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TriangleTypeIdentifierApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle Type Identifier");
+
+            Console.Write("Enter side 1: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side 2: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter side 3: ");
+            double side3 = Convert.ToDouble(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+            {
+                Console.WriteLine("The triangle is Equilateral.");
+            }
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+            {
+                Console.WriteLine("The triangle is Isosceles.");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is Scalene.");
+            }
+        }
+    }
+}
